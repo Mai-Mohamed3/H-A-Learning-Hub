ac565d5177f3001405e29992c9a8cfee24209703
Mai-Mohamed3 <mai.mo.alii3@gmail.com>
2026-06-12 14:56:21 +0300

    fix github pages paths

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/H-A-Learning-Hub/",
   plugins: [react()],
+  base: '/H-A-Learning-Hub/',
 })
